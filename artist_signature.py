64faa3ecbca2d6d8eb6ac650146be38e5de1efd6
Marc-Antoine Robin <marc-antoine.robin.1@ulaval.ca>
2019-12-18 10:17:21 -0500

    added position effect

--- artist_signature.py
+++ artist_signature.py
@@ -10,10 +10,10 @@ import inkex
 # The simplestyle module provides functions for style parsing.
 from simplestyle import *
 
-class ArtistSignature(inkex.Effect):
+class ArtistSignatureEffect(inkex.Effect):
     """
     Inkscape artist extension.
-    Creates signature on the document.
+    Creates artist's signature on the document.
     """
     def __init__(self):
         """
@@ -23,15 +23,16 @@ class ArtistSignature(inkex.Effect):
         # Call the base class constructor.
         inkex.Effect.__init__(self)
 
-        # Define string option "--what" with "-w" shortcut and default value "World".
+        # Define string option "--artname" with "-an" shortcut and default value "Artist Name"
         self.OptionParser.add_option('-an', '--artname', 
                                      action = 'store', type = 'string', 
-                                     dest = 'artist_name', default = 'Artist Name',
+                                     dest = 'artistName', default = 'Artist Name',
                                      help = "Enter your artist name")
 
+        # Define string option "--place" with "-p" shortcut and default value "bottomRight"
         self.OptionParser.add_option("-p", "--place",
                                      action="store", type="string", 
-                                     dest="signature_place", default='choice1',
+                                     dest="signaturePlace", default='bottomRight',
                                      help="Where do you want your signature to appear?")
 
     def effect(self):
@@ -39,30 +40,45 @@ class ArtistSignature(inkex.Effect):
         Effect behaviour.
         Overrides base class' method and inserts the artist's name text into SVG document.
         """
-        # Get script's "--what" option value.
-        what = self.options.what
+        # Get artist's name and signature place.
+        artistName = self.options.artistName
+        signaturePlace = self.options.signaturePlace
 
         # Get access to main SVG document element and get its dimensions.
         svg = self.document.getroot()
-        # or alternatively
-        # svg = self.document.xpath('//svg:svg',namespaces=inkex.NSS)[0]
 
-        # Again, there are two ways to get the attibutes:
+        # Get width and height:
         width  = self.unittouu(svg.get('width'))
         height = self.unittouu(svg.attrib['height'])
 
         # Create a new layer.
         layer = inkex.etree.SubElement(svg, 'g')
-        layer.set(inkex.addNS('label', 'inkscape'), 'Hello %s Layer' % (what))
+        layer.set(inkex.addNS('label', 'inkscape'), f'{artistName} signature layer')
         layer.set(inkex.addNS('groupmode', 'inkscape'), 'layer')
 
         # Create text element
         text = inkex.etree.Element(inkex.addNS('text','svg'))
-        text.text = 'Hello %s!' % (what)
+        text.text = artistName
 
-        # Set text position to center of document.
-        text.set('x', str(width / 2))
-        text.set('y', str(height / 2))
+        # Set text position.
+        if signaturePlace == "topLeft":
+            xPos = (width - width / 10)
+            yPos = (height - height / 10)   
+        elif signaturePlace == "topRight":
+            xPos = (width - width / 10)
+            yPos = (height - height / 10)
+        elif signaturePlace == "BottomLeft":
+            xPos = (width / 10)
+            yPos = (height / 10)
+        elif signaturePlace == "Center":
+            xPos = (width / 2)
+            yPos = (height / 2)
+        else:
+            xPos = (width - width/10)
+            yPos = (height/10)
+        
+        text.set('x', str(xPos))
+        text.set('y', str(yPos))
 
         # Center text horizontally with CSS style.
         style = {'text-align' : 'center', 'text-anchor': 'middle'}
@@ -72,5 +88,5 @@ class ArtistSignature(inkex.Effect):
         layer.append(text)
 
 # Create effect instance and apply it.
-effect = HelloWorldEffect()
+effect = ArtistSignatureEffect()
 effect.affect()
